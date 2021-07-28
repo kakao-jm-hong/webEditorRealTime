be7fa23c9f668f9080f13b65a739c4e44455d9f6
Hongjeongmin <wjdals0471@naver.com>
2021-07-29 00:20:15 +0900

    sass css loader 에러해결

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,21 @@ const path = require('path');
 module.exports = {
     entry:{
         index : "./src/index.js",
-        about : "./src/about.js",
     },
     output: {
         filename: '[name]_bundle.js',
         path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    `style-loader`,
+                    `css-loader`,
+                    `sass-loader`,
+                ]
+            }
+        ],
     }
 };
