5f9d7b15bc06dcdc22c91c8c6a2c08569b110529
Hongjeongmin <wjdals0471@naver.com>
2021-08-01 03:06:03 +0900

    kakao 로그인 구현 완료

--- back/index.js
+++ back/index.js
@@ -2,32 +2,84 @@ const express = require('express'); //express를 설치했기 때문에 가져�
 const app = express();
 const winston = require('winston');
 const logger = winston.createLogger();
-// const request = require('request');
+const qs = require('qs');
+const fetch = require('node-fetch');
 
-app.get(`/oauth/:coperation`, (req, res) => {
-    const coperation = req.params.coperation;
-    const code = req.param('code');
+class Kakao {
+    constructor(code) {
+        this.url = 'https://kauth.kakao.com/oauth/token';
+        this.clientID = '40743e16f1a2a35513c0d4bd149aa080';
+        this.clientSecret = 'riUIwNSJrzZQojJ2W7sH3kEo3Qdv2tM4';
+        this.redirectUri = 'http://localhost:8081/oauth/kakao';
+        this.code = code;
+
+        // userInfo
+        this.userInfoUrl = 'https://kapi.kakao.com/v2/user/me';
+    }
+}
+
+//  TODO Naver
+
+//  TODO Google
+
+const getAccessToken = async (options) => {
+    try {
+            return await fetch(options.url, {
+                method: 'POST',
+                headers: {
+                    'content-type':'application/x-www-form-urlencoded;charset=utf-8'
+                },
+                body: qs.stringify({
+                    grant_type: 'authorization_code',//특정 스트링
+                    client_id: options.clientID,
+                    client_secret: options.clientSecret,
+                    redirectUri: options.redirectUri,
+                    code: options.code,
+                }),
+            }).then(res => res.json());
+    }catch(e) {
+        logger.info("error", e);
+    }
+};
+
+const getUserInfo = async (url, access_token) => {
+    try {
+        return await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+                'Authorization': `Bearer ${access_token}`
+            }
+        }).then(res => res.json());
+    }catch(e) {
+        logger.info("error", e);
+    }
+};
+
+const getOption = (coperation, code)=> {
     switch(coperation){
         case 'kakao':
+            return new Kakao(code);
         break;
         case 'google':
+            // return new Google(code);
         break;
         case 'naver':
+            // return new Naver(code);
         break;
     }
+}
 
-    // const request = require('request');
+app.get(`/oauth/:coperation`, async (req, res) => {
+    const coperation = req.params.coperation;
+    const code = req.param('code');
+    const options = getOption(coperation, code);
+    const token = await getAccessToken(options);
+    const userInfo = await getUserInfo(options.userInfoUrl, token.access_token);
 
-    // const options = {
-    //     uri:'http://google.com',
-    //     method: 'POST',
-        
-    // }
-
-    // request.post(options, function (error, response, body) {
-    //     //callback
-    // });
-    res.send("test"+code);
+    // TODO Redirect Frot Server (쿠키, 세션, local_store 중에 로그인을 유지한다.)
+    // TODO Data Base or 쿠키 reflesh Token 저장 방법 모색
+    res.send(userInfo);
 })
 
 app.listen(8081);
