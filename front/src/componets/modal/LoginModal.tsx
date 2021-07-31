68739cb5d04c9f77a556286471b2a70d7f660f77
Hongjeongmin <wjdals0471@naver.com>
2021-07-31 19:16:57 +0900

    git ignore  적용

--- front/src/componets/modal/LoginModal.tsx
+++ front/src/componets/modal/LoginModal.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import style from './LoginModal.module.scss';
 import classnames from 'classnames';
+import { ReactComponent as Close }  from '../../images/svg/icon_close_search.svg';
 
 const LoginModal = ()=> {
     return (
         <div className={style.wrap}>
             <form className={style.inner}>
                 <label htmlFor="login_id" className="blind">ID</label>
-                <input id="login_id" type="text" className={classnames(style.id, style.box)}/>
+                <input id="login_id" type="text" placeholder="아이디" className={style.id}/>
                 <label htmlFor="login_password" className="blind">비밀번호</label>
-                <input id="login_password" type="password" className={classnames(style.password, style.box)}/>
-                <button type="submit" className={classnames(style.login, style.box)}>로그인</button>
-                <button className={classnames(style.kakao, style.box)}>카카오 로그인</button>
-                <button className={classnames(style.google, style.box)}>구글 로그인</button>
-                <button className={classnames(style.naver, style.box)}>네이버 로그인</button>
-                <button className={classnames(style.cancel, style.box)} aria-label="취소"></button>
+                <input id="login_password" type="password" placeholder="비밀번호" className={style.password}/>
+                <button type="submit" className={style.login}>로그인</button>
+                <button className={style.kakao}>카카오 로그인</button>
+                <button className={style.google}>구글 로그인</button>
+                <button className={style.naver}>네이버 로그인</button>
+                <button className={style.cancel} aria-label="취소"><Close width="18" height="18"/></button>
             </form>
         </div>
     );
