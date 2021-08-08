e4fae69396b4a052c9ede225849b91c831e93936
Hongjeongmin <wjdals0471@naver.com>
2021-08-09 00:31:38 +0900

    DataBase 연동

--- back/src/app.js
+++ back/src/app.js
@@ -3,6 +3,10 @@ const cors = require('cors');
 const app = express();
 const whitelist = ['http://localhost:3000'];
 
+// DB 설정
+const db_config = require(__dirname + '/config/database.js');
+const conn = db_config.init();
+
 const corsOptions = {
   origin: function(origin, callback){
   var isWhitelisted = whitelist.indexOf(origin) !== -1;
@@ -11,6 +15,14 @@ const corsOptions = {
   credentials:true
 };
 
+app.get('/test', function (req, res) {
+  console.log('tst');
+  var sql = 'SELECT * FROM Test';    
+  conn.query(sql, function (err, rows, fields) {
+      if(err) console.log('query is not excuted. select fail...\n' + err);
+      else res.send(rows);
+  });
+});
 
 const ouathRouter = require('./controller/oauth');
 const userRouter = require('./controller/user');
