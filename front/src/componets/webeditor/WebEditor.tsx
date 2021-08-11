b962f31bbce8a2696497f4656b5dbb5d48819e0d
Hongjeongmin <wjdals0471@naver.com>
2021-08-12 01:37:59 +0900

    디자인 추가

--- front/src/componets/webeditor/WebEditor.tsx
+++ front/src/componets/webeditor/WebEditor.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import classnames from "classnames";
-import style from './WebEditor.moudle.scss';
+import style from './WebEditor.module.scss';
+
 type Pprops = {
     className?: string;
-}
+};
 
 const WebEditor = ({className}: Pprops) => {
     return (
-        <div className={classnames(style.wrap, className)}>
-            WebEditor
+        <div className={classnames(className, style.wrap)}>
+            <span style={{color: '#1DDB16', fontWeight: 800}}># javaScript 테스트 코드입니다.</span><br></br>
+            <br></br>
+            <span style={{fontWeight:800}}>{'for (let i=0; i <10; i++)'}</span>
+            <span style={{color: "pink", fontWeight: 800}}>{' {'}</span>
+            <br></br>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <span style={{fontWeight:800, color:'#A0D9E2'}}>{'console'}</span>
+            <span style={{fontWeight:800, color:'#fff'}}>{'.'}</span>
+            <span style={{fontWeight:800, color:'#FAED7D'}}>{'log'}</span>
+            <span style={{fontWeight:800, color:'#FFE400'}}>{'('}</span>
+            <span style={{fontWeight:700, fontSize: 12, color: '#FFCD12'}}>{'\'Hello World!!\''}</span>
+            <span style={{fontWeight:800, color:'#FFE400'}}>{')'}</span>
+            <span style={{fontWeight:800, color:'#fff'}}>{';'}</span>
+            <br></br>
+            <span style={{color: "pink", fontWeight: 800}}>{' }'}</span>
         </div>
     )
 };
